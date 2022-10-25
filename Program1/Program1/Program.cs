7dc1b11792bb2bdf94ec255733bbc5f290f3217a
AccordioniTi <116587324+AccordioniTi@users.noreply.github.com>
2022-10-25 15:30:12 +0300

    Add files via upload

---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program1
+{
+    internal class Program
+    {
+       // Здесь программа начинает работу.
+       static void Main(string[] args)
+        {
+            // Приглашение пользователю ввести имя.
+            Console.WriteLine("Введите ваше имя:");
+            // Чтение введенного имени.
+            string name = Console.ReadLine();
+            // Приветствие пользователя по имени.
+            Console.WriteLine("Привет, " + name);
+            // Ожидание реакции пользователя.
+            Console.WriteLine("Нажмите <Enter> для выхода...");
+            Console.Read();
+        }
+    }
+}
