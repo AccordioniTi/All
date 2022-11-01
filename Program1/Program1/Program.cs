2624ea444b11484a91216127ed10565676d98f12
AccordioniTi <valerik_shef@mail.ru>
2022-11-01 17:03:31 +0300

    russion comment to en add int, bool, double, Datetime

--- Program1/Program1/Program.cs
+++ Program1/Program1/Program.cs
@@ -4,20 +4,24 @@ namespace Program1
 {
     internal class Program
     {
-       // Здесь программа начинает работу.
+       // program start working
        static void Main(string[] args)
         {
-            // Приглашение пользователю ввести имя.
+            // insert user name.
             Console.WriteLine("Введите ваше имя:");
-            // Чтение введенного имени.
+            // reading name.
             string name = Console.ReadLine();
-            // Приветствие пользователя по имени.
+            // user greeting.
+            int age = 25;
+            bool isEmployed = true;
+            double weight = 81.54;
+            DateTime date1 = new DateTime(1997, 1, 21);
             Console.WriteLine("Привет, " + name);
-            // your look
-            int m;
-            m = 100;
-            Console.WriteLine("Вы выглядите на все " + m );
-            // Ожидание реакции пользователя.
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"Работает: {isEmployed}");
+            Console.WriteLine($"Вес: {weight}");
+            Console.WriteLine($"Дата рождения: {date1.ToLongDateString()}");
+            // waiting user reaction.
             Console.WriteLine("Нажмите <Enter>\nдля выхода...");
             Console.Read();
         }
